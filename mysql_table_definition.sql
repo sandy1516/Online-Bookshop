bd9870f6019c093bbc08aea309cf493e643f5d18
sandeep kumar singh <sandeep.singh@minglebox.com>
2016-02-15 14:55:45 +0530

    updated

---
+++ mysql_table_definition.sql
@@ -0,0 +1,56 @@
+CREATE TABLE `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email_id` varchar(255) NOT NULL,
+  `mobile` bigint(50) DEFAULT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` enum('admin','user') NOT NULL DEFAULT 'admin',
+  `created_at` bigint(20) DEFAULT NULL,
+  `created_by` bigint(20) DEFAULT NULL,
+  `updated_at` bigint(20) DEFAULT NULL,
+  `updated_by` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `books` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `isbn` varchar(255) NOT NULL,
+  `book_name` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `publication` varchar(255) NOT NULL,
+  `edition` varchar(255) NOT NULL,
+  `cost` float(8,2) DEFAULT NULL,
+  `created_at` bigint(20) DEFAULT NULL,
+  `created_by` bigint(20) DEFAULT NULL,
+  `updated_at` bigint(20) DEFAULT NULL,
+  `updated_by` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `orders` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `number` varchar(50) DEFAULT NULL,
+  `address` varchar(255) NOT NULL,
+  `street` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `zip` varchar(255) NOT NULL,
+  `card_no` bigint(16) unsigned NOT NULL,
+  `card_expiry` varchar(255) NOT NULL,
+  `total_amount` float(8,2) DEFAULT NULL,
+  `order_date` varchar(255) NOT NULL,
+  `selected_number_Of_books` int(10) unsigned NOT NULL,
+  `order_status` enum('confirmed','processing','shipment','reached','delivered','cancelled') DEFAULT NULL,
+  `order_item_list` varchar(45) NOT NULL,
+  `created_at` bigint(20) DEFAULT NULL,
+  `created_by` bigint(20) DEFAULT NULL,
+  `updated_at` bigint(20) DEFAULT NULL,
+  `updated_by` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`,`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
